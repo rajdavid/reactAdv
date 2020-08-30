dad441f935380802a0a5cb8fc3e13957e8dac804
rajdavid <47979488+rajdavid@users.noreply.github.com>
2020-08-30 17:26:05 +0530

    Updates

---
+++ precache-manifest.430561dc4dc72894298c1f5590cedb76.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fa7e841094b880760ec0fe94b354efb7",
+    "url": "/reactAdv/index.html"
+  },
+  {
+    "revision": "900f1de3136f832090bf",
+    "url": "/reactAdv/static/css/main.eab24d3c.chunk.css"
+  },
+  {
+    "revision": "45aae3b9708aeff1c4c8",
+    "url": "/reactAdv/static/js/2.70249e97.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/reactAdv/static/js/2.70249e97.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "900f1de3136f832090bf",
+    "url": "/reactAdv/static/js/main.c66414b0.chunk.js"
+  },
+  {
+    "revision": "0d6a8c14546c7bee8130",
+    "url": "/reactAdv/static/js/runtime-main.bcd4fa94.js"
+  }
+]);
