e20658234fb6b552850d43ce1ae522f3a6bed99e
rajdavid <47979488+rajdavid@users.noreply.github.com>
2020-08-31 01:44:31 +0530

    render props

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import React from 'react';
 import './App.css';
-import ClickCounter from './components/ClickCounter';
-import HoverCounter from './components/HoverCounter';
+import Counter from './components/Counter';
+import ClickCounterTwo from './components/ClickCounterTwo';
+import HoverCounterTwo from './HoverCounterTwo';
+// import ClickCounterTwo from './components/ClickCounterTwo';
+// import HoverCounterTwo from './HoverCounterTwo';
+// import User from './components/User';
+//import ClickCounter from './components/ClickCounter';
+//import HoverCounter from './components/HoverCounter';
 // import Hero from './components/Hero';
 // import ErrorBoundry from './components/ErrorBoundry';
 // // import PortalDemo from './components/PortalDemo';
@@ -24,8 +30,25 @@ function App() {
       <ErrorBoundry>
         <Hero heroname='joker'/>
       </ErrorBoundry> */}
-      <ClickCounter name="david"/>
-      <HoverCounter/>
+      {/* <ClickCounter name="david"/>
+      <HoverCounter/> */}
+
+      {/* <ClickCounterTwo />
+      <HoverCounterTwo />
+      <User render={(isLoggedIn) => isLoggedIn ? 'david': 'guest'}/> */}
+
+      <Counter>
+      {(count, incrementcount) => (
+        <ClickCounterTwo count={count} incrementcount={incrementcount}/>
+      )}
+      </Counter>
+      <Counter>
+      {(count, incrementcount) => (
+        <HoverCounterTwo count={count} incrementcount={incrementcount}/>
+      )}
+
+      </Counter>
+
     </div>
   );
 }
